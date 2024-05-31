53d3549e7e34657830a62300b0c57d93660b7417
kostygovnv <kolyakostygov@gmail.com>
2024-05-31 10:21:51 +0300

    Add Unit Test

---
+++ Kostygov_Repozitory_v7_Test/Kostygov_Repozitory_v7_Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace KostygovRepozitoryv7Test
+{
+	TEST_CLASS(KostygovRepozitoryv7Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
